9a6e6537ac7b350f769888f5dbc47b78a75de8be
Yasuhiro Usutani <y_usutani@mac.com>
2020-05-25 11:12:46 +0900

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
-  resources :users
   root 'home#index'
+
+  resources :users
+  resources :sessions, only: %i[new create destroy]
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
 end
