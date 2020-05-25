dcbca1669b058d713919d331537923b8184e6790
Yasuhiro Usutani <y_usutani@mac.com>
2020-05-25 11:30:59 +0900

    remote-form_with

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
       redirect_to root_url, notice: 'Logged in!'
     else
       flash.now[:alert] = 'Email or password is invalid'
-      render 'new'
+      render 'create'
     end
   end
 
